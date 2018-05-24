a22383575acdad1f069c1efb87c85a5ac9b22893
Harikiran <harikiran.s@thresholdsoft.com>
2018-05-24 11:17:38 +0530

    DataGrid

---
+++ projects/tss-grid/src/lib/tss-grid.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TssGridComponent } from './tss-grid.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [TssGridComponent],
+  exports: [TssGridComponent]
+})
+export class TssGridModule { }
---
+++ projects/tss-grid/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of tss-grid
+ */
+
+export * from './lib/tss-grid.service';
+export * from './lib/tss-grid.component';
+export * from './lib/tss-grid.module';
